05fb1ca5645c48a71fd19cc72852575b377f5c31
B3nnyL <B3nnyL@users.noreply.github.com>
2024-10-12 22:20:30 +1100

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { columns } from "@/components/parts/columns"
 import { DataTable } from "@/components/parts/data-table"
 import { aiSecNewschema } from "@/data/schema"
 import { Toaster } from "@/components/ui/toaster"
+import logo from '@/app/logo.png'
 
 
 async function getaiSecNews() {
@@ -28,14 +29,18 @@ export default async function LeakPage() {
       <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
         <div className="flex items-center justify-between space-y-2">
           <div>
-            <h2 className="text-2xl font-bold tracking-tight">AISec.fyi</h2>
+            <div className="flex items-center gap-2">
+              <Image src={logo} alt="logo" width={32} height={32} />
+              <h2 className="text-2xl font-bold tracking-tight">AISec.fyi</h2>
+              </div>
+
             <p className="text-muted-foreground">
               Here&apos;s a list of AI security, privacy breach, data leak and other related information
             </p>
           </div>
         </div>
         <DataTable data={data} columns={columns} />
-        <Toaster/>
+        <Toaster />
       </div>
     </>
   )
