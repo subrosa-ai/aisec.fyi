f6683b8dd70fc961497d3284cef843757193a606
B3nnyL <B3nnyL@users.noreply.github.com>
2024-10-16 17:46:16 +1100

    fix lastUpdated issue

--- src/pages/api/last-updated.ts
+++ src/pages/api/last-updated.ts
@@ -1,14 +1,51 @@
-import { promises as fs } from "fs";
-import path from "path";
-import { NextApiRequest, NextApiResponse } from 'next'; // Import types
+import { NextApiRequest, NextApiResponse } from 'next'; 
+import https from 'https';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) { // Explicitly type parameters
+const REPO_OWNER = 'subrosa-ai'; 
+const REPO_NAME = 'aisec.fyi'; 
+const FILE_PATH = 'src/data/updates.json'; 
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) { 
   try {
-    const filePath = path.join(process.cwd(), "src/data/updates.json");
-    const stats = await fs.stat(filePath);
-    res.status(200).json({ lastUpdated: stats.mtime.toDateString() });
+    const commitDate = await getLatestCommitDate();
+    const dateString = new Date(commitDate).toDateString()
+    res.status(200).json({ lastUpdated: dateString });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({ error: "Failed to fetch last updated time" });
   }
+}
+// Function to get the latest commit date from GitHub
+function getLatestCommitDate(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const url = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/commits?path=${FILE_PATH}`;
+    const options = {
+      headers: {
+        'User-Agent': 'Node.js'
+      }
+    };
+
+    https.get(url, options, (res) => {
+      let data = '';
+
+      // Collect data chunks
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // On end of response
+      res.on('end', () => {
+        const commits = JSON.parse(data);
+        console.log(commits[0].commit.committer)
+        if (commits.length > 0) {
+          resolve(commits[0].commit.committer.date);
+        } else {
+          resolve('No commits found for this file.');
+        }
+      });
+    }).on('error', (err) => {
+      reject('Error fetching data from Github: ' + err.message);
+    });
+  });
 }
